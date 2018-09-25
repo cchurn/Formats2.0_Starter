11fd10b1af340c806a50c8a66453749968137da7
cchurn16 <christopher.churn@oath.com>
2018-09-25 16:24:01 +0100

    updated webpack configs

--- src/webpack_configs/all_inline.js
+++ src/webpack_configs/all_inline.js
@@ -1,5 +1,5 @@
 /**
-    ALL INLINE
+ ALL INLINE
  */
 // @remove-on-eject-begin
 /**
@@ -25,6 +25,7 @@ const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const ZipPlugin = require('zip-webpack-plugin');
 const tinyPngWebpackPlugin = require('tinypng-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 const publicPath = paths.servedPath;
@@ -47,7 +48,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].css';
+const cssFilename = '[name].css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
@@ -75,8 +76,8 @@ module.exports = {
         // Generated JS file names (with nested folders).
         // There will be one main bundle, and one file per asynchronous chunk.
         // We don't currently advertise code splitting but Webpack supports it.
-        filename: 'static/js/[name].js',
-        chunkFilename: 'static/js/[name].chunk.js',
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js',
         // We inferred the "public path" (such as / or /my-project) from homepage.
         publicPath: publicPath,
         // Point sourcemap entries to original disk location (format as URL on Windows)
@@ -168,7 +169,7 @@ module.exports = {
                         loader: require.resolve('url-loader'),
                         options: {
                             limit: 10000000,
-                            name: 'static/media/[name].[ext]',
+                            name: '[name].[ext]',
                         },
                     },
                     // Process JS with Babel.
@@ -242,7 +243,7 @@ module.exports = {
                         // by webpacks internal loaders.
                         exclude: [/\.js$/, /\.html$/, /\.json$/],
                         options: {
-                            name: 'static/media/[name].[ext]',
+                            name: '[name].[ext]',
                         },
                     },
                     // ** STOP ** Are you adding a new loader?
@@ -305,12 +306,12 @@ module.exports = {
         // Generate a manifest file which contains a mapping of all asset filenames
         // to their corresponding output file so that tools can pick it up without
         // having to parse `index.html`.
-      /*new ManifestPlugin({
-       fileName: 'asset-manifest.json',
-       }),*/
+        /*new ManifestPlugin({
+         fileName: 'asset-manifest.json',
+         }),*/
         // Generate a service worker script that will precache, and keep up to date,
         // the HTML & assets that are part of the Webpack build.
-        new SWPrecacheWebpackPlugin({
+        /*new SWPrecacheWebpackPlugin({
             // By default, a cache-busting query parameter is appended to requests
             // used to populate the caches, to ensure the responses are fresh.
             // If a URL is already hashed by Webpack, then there is no concern
@@ -334,17 +335,20 @@ module.exports = {
             navigateFallback: publicUrl + '/index.html',
             // Ignores URLs starting from /__ (useful for Firebase):
             // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-            navigateFallbackWhitelist: [/^(?!\/__).*/],
+            navigateFallbackWhitelist: [/^(?!\/__).*!/],
             // Don't precache sourcemaps (they're large) and build asset manifest:
             staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-        }),
+        }),*/
         // Moment.js is an extremely popular library that bundles large locale files
         // by default due to how Webpack interprets its code. This is a practical
         // solution that requires the user to opt into importing specific locales.
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
+        new tinyPngWebpackPlugin({
+            key:"tp58A84Qs2_Jk7GNlelBUJeyIrFk8s9p",
+            ext: ['png', 'jpeg', 'jpg']
+        }),
         new ZipPlugin({
             // OPTIONAL: defaults to the Webpack output path (above)
             // can be relative (to Webpack output path) or absolute
@@ -395,10 +399,7 @@ module.exports = {
                 forceZip64Format: false,
             },
         }),
-        new tinyPngWebpackPlugin({
-            key:"tp58A84Qs2_Jk7GNlelBUJeyIrFk8s9p",
-            ext: ['png', 'jpeg', 'jpg']
-        })
+        new BundleAnalyzerPlugin()
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
