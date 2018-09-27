1cb423c07721f374e6c2d9e8f4d4fb5e0e7785a1
cchurn16 <christopher.churn@oath.com>
2018-09-27 11:06:06 +0100

    lazyload

--- src/creative/Creative.js
+++ src/creative/Creative.js
@@ -1,6 +1,8 @@
 /*global*/
 import Display from './Display';
 import Layout from '../core/Layout';
+import loadJS from 'load-js';
+import lazyLoadCSS from 'lazyload-css';
 
 var creative = {
     defaults: {
@@ -10,7 +12,7 @@ var creative = {
     config: {container: '', responsive: false, width: 320, height: 568},
     init: function(opts) {
 
-        for(var i in opts){
+        for(var i in opts) {
             this.options[i] = {
                 value: opts[i],
                 enumerable: true,
@@ -20,15 +22,62 @@ var creative = {
         }
         this.config = Object.create(this.defaults, this.options);
         console.log('creative init', this.config);
+        let layout = new Layout();
         if (this.config.responsive) {
-            let layout = new Layout();
             layout.init(this.config);
         } else {
             document.querySelector(this.config.container).style.width = this.config.width + 'px';
             document.querySelector(this.config.container).style.height = this.config.height + 'px';
         }
-        let visualisation = new Display();
-        visualisation.init(this.config);
+
+        /**
+         * Lazy loading
+         * If any JS or CSS files are passed via the config they'll be loaded here
+         * before Display.js starts
+         */
+        let arr_scripts = this.config.lazyLoad;
+        let loadJSArray = [];
+        let loadCSSArray = [];
+        let hasInit;
+        if (arr_scripts && arr_scripts.length) {
+            for (let value of arr_scripts) {
+                switch (value.split('.').pop()) {
+                    case 'js':
+                        let loadObj = {async: false, url: value};
+                        loadJSArray.push(loadObj);
+                        break;
+                    case 'css':
+                        const promise = lazyLoadCSS(value);
+                        loadCSSArray.push(promise);
+                        break;
+                }
+            }
+            Promise.all(loadCSSArray).then(res => console.log(res));
+            loadJS( loadJSArray )
+                .then(() => {
+                    console.log("lazy loaded " + arr_scripts.length + " scripts");
+                    if (layout.isPortrait()) {
+                        this.startDisplay();
+                    } else {
+                        // this prevents initialising until in portrait aspect
+                        layout.on('layout_resize', ()=> {
+                            if (hasInit) return;
+                            if (layout.isPortrait()) {
+                                hasInit = true;
+                                this.startDisplay();
+                            }
+                        });
+                    }
+                });
+        }
+        // end lazy loading
+        else {
+            this.startDisplay();
+        }
+    },
+    startDisplay: function() {
+        let display = new Display();
+        display.init(this.config);
     }
 };
 
